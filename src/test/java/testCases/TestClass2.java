8edacafb631b330a128b244b1d9f9fb312f9dc7b
Lokesh Kumar <kumarlokesh57@gmail.com>
2022-02-11 15:23:50 +0530

    added TestClass2

---
+++ src/test/java/testCases/TestClass2.java
@@ -0,0 +1,14 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class TestClass2 {
+	
+	@Test
+	public void methodinTestClass2() {
+		
+		System.out.println("inside methodinTestClass2 ");
+		 
+	}
+
+}
